1e77cb4499ae892e14b24b99150f0d9abb4cd90b
Viktor <komvik.slack@gmail.com>
2024-10-30 14:31:30 +0100

    actual code 30.10 __ 14:31

--- lib/feature/authorization/screens/registration_data_screen.dart
+++ lib/feature/authorization/screens/registration_data_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:projekt_481_play_with_me/feature/authorization/screens/authorization_screen.dart';
-import 'package:projekt_481_play_with_me/feature/authorization/widgets/create_input_text_field.dart';
+//import 'package:projekt_481_play_with_me/feature/authorization/screens/authorization_screen.dart';
+//import 'package:projekt_481_play_with_me/feature/authorization/widgets/create_input_text_field.dart';
 import 'package:projekt_481_play_with_me/feature/authorization/repositories/avatar_images.dart';
 
 class RegistrationDataScreen extends StatefulWidget {
@@ -32,139 +32,125 @@ class _RegistrationDataScreenState extends State<RegistrationDataScreen> {
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
-          scale: 0.5,
           image: AssetImage("assets/images/background3.png"),
           fit: BoxFit.cover,
         ),
       ),
       child: Scaffold(
-        backgroundColor: const Color.fromRGBO(0, 0, 0, 0),
+        backgroundColor: const Color.fromARGB(0, 255, 255, 255),
         body: Container(
           width: 430,
           height: 900,
           padding:
               const EdgeInsets.only(top: 130, bottom: 0, left: 0, right: 0),
-          child: Stack(
-            textDirection: TextDirection.ltr,
-            children: <Widget>[
-              Positioned(
-                top: 1,
-                left: 320,
-                child: IconButton(
-                  iconSize: 48,
-                  icon: const Icon(Icons.photo_library, color: Colors.blue),
-                  onPressed: _pickImage, // Вызов функции для выбора изображения
-                  tooltip: 'Wählen Sie ein Foto aus der Galerie aus',
+          child: Form(
+            child: Stack(
+              children: const <Widget>[
+                Positioned(
+                  top: 1,
+                  left: 320,
+                  child: IconButton(
+                    iconSize: 48,
+                    icon: Icon(Icons.photo_library, color: Colors.blue),
+                    onPressed:
+                        _pickImage, // Вызов функции для выбора изображения
+                    tooltip: 'Wählen Sie ein Foto aus der Galerie aus',
+                  ),
                 ),
-              ),
-              const Positioned(
-                top: 15,
-                left: 110,
-                child: Text("Erstelle deinen eigenen Avatar"),
-              ),
-              Positioned(
-                top: 50,
-                left: 130,
-                child: CircleAvatar(
-                  radius: 90,
-                  backgroundImage: ExactAssetImage(_selectedAvatar),
+                // Text oben
+                Positioned(
+                  top: 2,
+                  left: 110,
+                  child: Text("Erstelle deinen eigenen Avatar"),
                 ),
-              ),
-              //___________________________ List Avatar
-              Positioned(
-                top: 220,
-                left: 85,
-                child: SizedBox(
-                  width: 250,
-                  height: 100,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: avatars.length,
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _selectedAvatar = avatars[index];
+                // Avatar
+                Positioned(
+                  top: 30,
+                  left: 140,
+                  child: CircleAvatar(
+                    radius: 70,
+                    backgroundImage: ExactAssetImage(_selectedAvatar),
+                  ),
+                ),
 
-                            /// Update the selected avatar
-                          });
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 5),
-                          child: CircleAvatar(
-                            radius: 30,
-                            backgroundImage: ExactAssetImage(avatars[index]),
+                // List Avatar
+                Positioned(
+                  top: 150,
+                  left: 85,
+                  child: SizedBox(
+                    width: 250,
+                    height: 100,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: avatars.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _selectedAvatar = avatars[index];
+
+                              /// Update the selected avatar
+                            });
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 5),
+                            child: CircleAvatar(
+                              radius: 30,
+                              backgroundImage: ExactAssetImage(avatars[index]),
+                            ),
                           ),
-                        ),
-                      );
-                    },
+                        );
+                      },
+                    ),
                   ),
                 ),
-              ),
-              //____________________________ name
-              Positioned(
-                top: 310,
-                left: 40,
-                child: CreateInputTextField(
-                  onChange: (value) {},
-                  inputHeight: 50,
-                  inputWidth: 350,
-                  inputRadius: 10.0,
-                  label: "Geben Sie Ihren Namen ein",
-                  inputObscureText: false,
-                ),
-              ),
-              //____________________________ Nachname
-              Positioned(
-                top: 370,
-                left: 40,
-                child: CreateInputTextField(
-                  onChange: (value) {},
-                  inputHeight: 50,
-                  inputWidth: 350,
-                  inputRadius: 10.0,
-                  label: "Geben Sie Ihren Nachnamen ein",
-                  inputObscureText: false,
-                ),
-              ),
-              //____________________________ Spitzname
-              const Positioned(
-                top: 420,
-                left: 320,
-                child: Text(
-                  "optional",
-                  style: TextStyle(color: Color.fromARGB(255, 213, 226, 228)),
-                ),
-              ),
-              Positioned(
-                top: 440,
-                left: 40,
-                child: CreateInputTextField(
-                  onChange: (value) {},
-                  inputHeight: 50,
-                  inputWidth: 350,
-                  inputRadius: 10.0,
-                  label: "Geben Sie Ihren Spitznamen ein",
-                  inputObscureText: false,
-                ),
-              ),
-              //____________________________ Widerhollen pass
-              Positioned(
-                top: 645,
-                left: 140,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const AuthorizationScreen()));
-                  },
-                  child: Text(
-                    " Profil speichern",
-                    style: Theme.of(context).textTheme.displayMedium,
+                //==============================================================
+                //===========================================  Begin TextFields
+                //____________________________ name
+                 Positioned(
+                  top: 310,
+                  left: 40,
+                  right: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      label: Text("Name"),
+                      labelStyle: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                      border: OutlineInputBorder(
+
+
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              //_________________________________________
-            ],
+                //____________________________ Nachname
+                Positioned(
+                  top: 360,
+                  left: 40,
+                  right: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      label: Text("Name"),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+
+                //____________________________ Spitzname optionl
+                Positioned(
+                  top: 410,
+                  left: 40,
+                  right: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      label: Text("Name"),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
